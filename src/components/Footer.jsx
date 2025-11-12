dd7cdb7a752e180bb8b07e3381aac9367e0aa888
Miguel <miguel911201@gmail.com>
2025-11-11 21:29:05 -0600

    NavegaLasRutas+MiguelDiaz

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,13 +5,13 @@ function Footer() {
     <footer className="footer">
       <div className="social-icons">
         <a href="https://www.facebook.com/">
-          <img src="./public/IMG/facebook.png" alt="Facebook" />
+          <img src="../public/IMG/facebook.png" alt="Facebook" />
         </a>
         <a href="https://x.com/">
-          <img src="./public/IMG/Twitter.png" alt="Twitter" />
+          <img src="../public/IMG/Twitter.png" alt="Twitter" />
         </a>
         <a href="https://www.instagram.com/">
-          <img src="./public/IMG/Instagram.jpg" alt="Twitter" />
+          <img src="../public/IMG/Instagram.jpg" alt="Twitter" />
         </a>
       </div>
       <p>© 2025 Mi Tienda. Todos los derechos reservados.</p>
