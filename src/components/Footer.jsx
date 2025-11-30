79812ce5b06809103c8ef852c6700f534f319014
Miguel <miguel911201@gmail.com>
2025-11-29 22:44:31 -0600

    Quitando 1 punto a los link del footer, para ver si funcionan las imagenes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ function Footer() {
           <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Logo_of_Twitter.svg/250px-Logo_of_Twitter.svg.png" alt="Twitter" />
         </a>
         <a href="https://www.instagram.com/">
-          <img src=".https://1000marcas.net/wp-content/uploads/2019/11/Instagram-Logo-2016.png" alt="Twitter" />
+          <img src="https://1000marcas.net/wp-content/uploads/2019/11/Instagram-Logo-2016.png" alt="Twitter" />
         </a>
       </div>
       <p>© 2025 Mi Tienda. Todos los derechos reservados.</p>
