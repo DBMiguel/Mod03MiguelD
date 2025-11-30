8ab7715c60f6ac2efe8fd0d98488af5d0069daf2
Miguel <miguel911201@gmail.com>
2025-11-29 22:40:46 -0600

    Haciendo prueba con las imagenes de footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     <footer className="footer">
       <div className="social-icons">
         <a href="https://www.facebook.com/">
-          <img src="./public/IMG/facebook.png" alt="Facebook" />
+          <img src="https://img.freepik.com/vector-premium/ilustracion-vectorial-icono-redes-sociales-facebook_534308-21672.jpg?semt=ais_hybrid&w=740&q=80" alt="Facebook" /> // Agregando este link para hacer prueba
         </a>
         <a href="https://x.com/">
           <img src="./public/IMG/Twitter.png" alt="Twitter" />
