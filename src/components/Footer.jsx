5ba984b787535e83ab98dbb33fe85d19b5381e03
Miguel <miguel911201@gmail.com>
2025-11-29 22:42:59 -0600

    Haciendo prueba con las imagenes de footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,13 +5,13 @@ function Footer() {
     <footer className="footer">
       <div className="social-icons">
         <a href="https://www.facebook.com/">
-          <img src="https://img.freepik.com/vector-premium/ilustracion-vectorial-icono-redes-sociales-facebook_534308-21672.jpg?semt=ais_hybrid&w=740&q=80" alt="Facebook" /> // Agregando este link para hacer prueba
+          <img src="https://img.freepik.com/vector-premium/ilustracion-vectorial-icono-redes-sociales-facebook_534308-21672.jpg?semt=ais_hybrid&w=740&q=80" alt="Facebook" /> 
         </a>
         <a href="https://x.com/">
-          <img src="./public/IMG/Twitter.png" alt="Twitter" />
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Logo_of_Twitter.svg/250px-Logo_of_Twitter.svg.png" alt="Twitter" />
         </a>
         <a href="https://www.instagram.com/">
-          <img src="./public/IMG/Instagram.jpg" alt="Twitter" />
+          <img src=".https://1000marcas.net/wp-content/uploads/2019/11/Instagram-Logo-2016.png" alt="Twitter" />
         </a>
       </div>
       <p>© 2025 Mi Tienda. Todos los derechos reservados.</p>
